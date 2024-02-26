f953fb9e204e62200d547c2f9ebb9ce08e418b35
Annaliza_C <aldcadiente@gmail.com>
2024-02-25 17:43:42 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var shoutIt = function (name) {
+  var shout = `Hello, ${name}, nice to meet you!`.toUpperCase();
+  return shout;
+};
+
+console.log(shoutIt("Annaliza"));
+console.log(shoutIt("Kevin"));
+console.log(shoutIt("Febo"));
+console.log(shoutIt("Jenny"));
